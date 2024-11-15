d66f118fa74952f62f8a5cc9a7835a4ad76236e8
hanjyoon01 <hanjyoon@naver.com>
2024-11-15 12:29:08 +0900

    .

--- src/MafiaServer.java
+++ src/MafiaServer.java
@@ -19,29 +19,29 @@ public class MafiaServer {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("Server started. Waiting for players...");
 
-            Socket[] clientSocket = new Socket[4];
-            BufferedReader[] inStreams = new BufferedReader[4];
-            PrintWriter[] outStreams = new PrintWriter[4];
+//            Socket[] clientSocket = new Socket[4];
+//            BufferedReader[] inStreams = new BufferedReader[4];
+//            PrintWriter[] outStreams = new PrintWriter[4];
 
-            // 4명의 플레이어 접속 대기
-            for (int i = 0; i < clientSocket.length; i++) {
-                clientSocket[i] = serverSocket.accept();
-                inStreams[i] = new BufferedReader(new InputStreamReader(clientSocket[i].getInputStream()));
-                outStreams[i] = new PrintWriter(clientSocket[i].getOutputStream(), true);
-                outStreams[i].println("Welcome Player " + (i + 1) + "!");
-                System.out.println("Player " + (i + 1) + " connected.");
-                ClientHandler clientHandler = new ClientHandler(clientSocket[i]);
-                clients.add(clientHandler);
-                clientHandler.start();
-            }
-
-//            while (true) {
-//                Socket clientSocket = serverSocket.accept();
-//                ClientHandler clientHandler = new ClientHandler(clientSocket);
+//            // 4명의 플레이어 접속 대기
+//            for (int i = 0; i < clientSocket.length; i++) {
+//                clientSocket[i] = serverSocket.accept();
+//                inStreams[i] = new BufferedReader(new InputStreamReader(clientSocket[i].getInputStream()));
+//                outStreams[i] = new PrintWriter(clientSocket[i].getOutputStream(), true);
+//                outStreams[i].println("Welcome Player " + (i + 1) + "!");
+//                System.out.println("Player " + (i + 1) + " connected.");
+//                ClientHandler clientHandler = new ClientHandler(clientSocket[i]);
 //                clients.add(clientHandler);
 //                clientHandler.start();
-//                System.out.println("New player connected.");
 //            }
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                clients.add(clientHandler);
+                clientHandler.start();
+                System.out.println("New player connected.");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
